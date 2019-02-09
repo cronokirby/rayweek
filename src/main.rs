7729103000158bac3bc01afb322b051fad1fff51
Lúcás Meier <cronokirby@gmail.com>
2019-02-09 19:42:20 +0100

    Depend on the image crate

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 use std::io;
-use std::fs::File;
 
-mod image;
-use image::*;
+extern crate image;
+use image::{ImageBuffer, Rgba};
+
+
+fn clip(f: f32) -> f32 {
+    if f > 1.0 {
+        1.0
+    } else if f < 0.0 {
+        0.0
+    } else {
+        f
+    }
+}
+
+fn color(r: f32, g: f32, b: f32, a: f32) -> Rgba<u8> {
+    Rgba([
+        (clip(r) * 255.0) as u8,
+        (clip(g) * 255.0) as u8,
+        (clip(b) * 255.0) as u8,
+        (clip(a) * 255.0) as u8
+    ])
+}
 
 
 fn main() -> io::Result<()> {
-    let file = File::create("image.bmp")?;
-    let image = Image::solid_color(20, 20, RGBA(0.0, 0.5, 1.0, 1.0));
-    image.write_bmp(file)?;
+    let img = ImageBuffer::from_fn(1024, 768, |x, y| {
+        let r = (x as f32) / 1024.0;
+        let g = (y as f32) / 768.0;
+        color(r, g, 0.2, 1.0)
+    });
+    img.save("image.png")?;
     Ok(())
 }
